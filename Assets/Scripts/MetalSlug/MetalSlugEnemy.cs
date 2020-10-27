53fb32b177aede7c9eac4649a96c2773874c0bde
andres10 <andres@piola.dev>
2020-10-27 19:47:06 -0300

    Initial commit

---
+++ Assets/Scripts/MetalSlug/MetalSlugEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalSlugEnemy : MonoBehaviour
+{
+
+    public GameObject targetPlayer;
+    
+  
+    public void OnPlayerDetected(GameObject detectedPlayer)
+    {
+        targetPlayer = detectedPlayer;
+    }
+
+    void ShootPlayer()
+    {
+
+        if(targetPlayer != null)
+        {
+             print("shoot player");
+        }
+         
+          
+        
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ShootPlayer();
+    }
+}
