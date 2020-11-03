ce87cfafd8d2035a2b2738a439a383a6ef32a744
DavidQuesillo <68030286+DavidQuesillo@users.noreply.github.com>
2020-11-03 17:53:36 -0300

    clase martes 3

---
+++ Assets/Scripts/MetalSlug/AutoDisable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDisable : MonoBehaviour
+{
+    public float AutoDisableTime = 2f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(AutoDisableWithTime());
+    }
+
+    IEnumerator AutoDisableWithTime()
+    {
+        yield return new WaitForSeconds(AutoDisableTime);
+
+        gameObject.SetActive(false);
+    }
+}
