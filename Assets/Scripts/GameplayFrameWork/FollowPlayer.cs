cb911f189190f291cb4be3f4c8226a0a2fd35cc0
andres10 <andres@piola.dev>
2020-11-04 15:32:03 -0300

    player follow

---
+++ Assets/Scripts/GameplayFrameWork/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Transform playerTransform;
+    public float moveSpeed = 1; 
+
+    private void OnEnable()
+    {
+        if(playerTransform==null)
+        {
+            playerTransform= GameObject.FindGameObjectWithTag("Player").transform;
+
+        }
+
+        MoveToPlayer();
+    }
+   
+    void MoveToPlayer()
+    {
+        GetComponent<Rigidbody2D>().DOMove(playerTransform.position, 1 / moveSpeed).OnComplete(PathFinish);
+    }
+
+    void PathFinish()
+    {
+        
+    }
+}
