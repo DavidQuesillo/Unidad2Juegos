8a0acabad6ded19d3d3e70239dfa71764e344e19
andres10 <andres@piola.dev>
2020-11-10 14:36:35 -0300

    added UI score and Score in GameManager

---
+++ Assets/Scripts/GameplayFrameWork/AddScoreToGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScoreToGameManager : MonoBehaviour
+{
+    public int baseScore=1;
+    public void AddScore( )
+    {
+        GameManager.Instance?.AddScore(baseScore);
+
+    }
+    public void AddScoreCustomValue(int ScoreToAdd)
+    {
+        GameManager.Instance?.AddScore(ScoreToAdd);
+
+    }
+    public void RestScore(int ScoreToAdd)
+    {
+        GameManager.Instance?.AddScore(-ScoreToAdd);
+    }
+}
---
+++ Assets/Scripts/GameplayFrameWork/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int score;
+
+    public static event FNotify_1Params<int> OnScoreChange;
+
+    public void AddScore(int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+        OnScoreChange?.Invoke(score);
+    }
+}
